03d0c8aa0b8a0be56cc382776a5aa455c652681f
paheseb <pahesebw@gmail.com>
2022-08-06 00:22:41 -0500

    Login Files added to application

---
+++ src/components/RequireAuth/RequireAuth.jsx
@@ -0,0 +1,15 @@
+import { Outlet, Navigate, useLocation } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
+const RequiredAuth = () => {
+  const { auth } = useAuth();
+  const location = useLocation();
+
+  return auth?.username ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+};
+
+export default RequiredAuth;
