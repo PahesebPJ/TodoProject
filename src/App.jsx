82614c98bd4eeab0e0d7c2c9f21c32d1e61aefc2
paheseb <pahesebw@gmail.com>
2022-08-06 02:18:17 -0500

    Added ProjectsPage

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Layout from "./components/Layout/Layout";
 
 //Pages
 import Login from "./pages/Login/Login";
+import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
+import TaskPage from "./pages/TaskPage/TaskPage";
 
 function App() {
   return (
@@ -18,6 +21,10 @@ function App() {
           <Route path="login" element={<Login />} />
 
           {/*PRIVATE ROUTES*/}
+          <Route element={<RequireAuth />}>
+            <Route path="projects" element={<ProjectsPage />} />
+            <Route path="task" element={<TaskPage />} />
+          </Route>
         </Route>
       </Routes>
     </div>
